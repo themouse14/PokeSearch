1445653687a03cf438518fc25f59ed79093767d2
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-08 21:14:56 -0700

    idk what I'm doing

--- assets/script.js/pokeAPI.js
+++ assets/script.js/pokeAPI.js
@@ -23,6 +23,7 @@ testCall();
 
 const pokemonSearch = document.querySelector("#searchBtn")
 const dataTable = document.getElementById("datatable")
+let getPokemonImage 
 
 async function getPokemon() {
     const searchValue = document.getElementById("datatable-search-input").value;
@@ -30,31 +31,60 @@ async function getPokemon() {
     await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue.toString() + "/" )
     .then(response => response.json())
     .then(pokemon => {
-            console.log(pokemon.abilities);
+            console.log(pokemon);
+            
+            const pokemonId = pokemon.id;
+            console.log(pokemonId);
+
+            const pokeNum = document.querySelector(".num-container");
+            const pokemonNum = document.createElement("h2");
+            pokeNum.appendChild(pokemonNum);
+            pokemonNum.textContent = "#" + pokemonId;
+            //unable to capture data for image
+            // pokemonImage = pokemon.sprites.back_default
+            // console.log(pokemonImage);
+            
+
+            //Dynamically appending pokemon name to html 
+            const pokeName = pokemon.name;
+            console.log(pokeName);
+
+
+            const nameContainer = document.querySelector(".name-container");
+            const pokemonName = document.createElement("h1");
+            nameContainer.appendChild(pokemonName);
+            pokemonName.textContent = pokeName;
+            
+            
+
+            // nameContainer.textContent= "";
+            // pokemonName.textContent = pokeName;
+            
+
 
             
-            // for (let j = 0; j < data.results.length; j++) {
-                //     console.log(data.results[j].name)
-                const abilityContainer = document.querySelector(".ability-container")
-                const pokemonInfo = document.createElement("div")
-                abilityContainer.textContent = ""
-                pokemonInfo.setAttribute("class", "pokemoninfo")
-                // pokemonInfo.textContent = 
-                console.log(pokemonInfo)
-                abilityContainer.appendChild(pokemonInfo)
+            
+            
+            
+
+
+            //Dynamically appending pokemon abilities to html
+            const pokemonInfo = document.createElement("div")
+            pokemonInfo.setAttribute("class", "pokemoninfo")
+            console.log(pokemonInfo)
+
+            //abilities
+            const abilityContainer = document.querySelector(".ability-container")
+            abilityContainer.textContent = "";
+            abilityContainer.appendChild(pokemonInfo)
 
                 for (let i = 0; i < pokemon.abilities.length; i++) {
                     console.log(pokemon.abilities[i].ability.name);
                     const ability = document.createElement("p")
                     ability.textContent = pokemon.abilities[i].ability.name
-                    pokemonInfo.appendChild(ability)
-                }
-                
-                
-
-                
-        })
-    }
+                    pokemonInfo.appendChild(ability)}
+    })
+}
 
 
 
