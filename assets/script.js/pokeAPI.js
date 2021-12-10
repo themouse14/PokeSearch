a3bde54be1d3d05db9dceb5ad1c9f377b812c733
David Gavino <ggavino2@uccs.edu>
2021-12-09 17:48:38 -0700

    modified js

--- assets/script.js/pokeAPI.js
+++ assets/script.js/pokeAPI.js
@@ -9,17 +9,7 @@ async function testCall() {
 
 testCall();
 
-// Second API Call
-// API Key: 4c3f5891-21b8-4f5e-892e-a14841e797e7
-
-// async function testCall2() {
-//     await fetch("https://api.pokemontcg.io/v2/cards")
-//         .then(response => response.json())
-//         .then(types => console.log(types))
-
-// }
-
-// testCall2();
+        
 
 const pokedex = document.getElementById('pokedex');
 
@@ -59,44 +49,44 @@ const displayPokemon = (pokemon) => {
 
 fetchPokemon();
 
-// const pokemonSearch = document.querySelector("#searchBtn")
-// const dataTable = document.getElementById("datatable")
+const pokemonSearch = document.querySelector("#searchBtn")
+const dataTable = document.getElementById("datatable")
 
-// async function getPokemon() {
-//     const searchValue = document.getElementById("datatable-search-input").value;
-//     console.log(searchValue)
-//     await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue.toString() + "/" )
-//     .then(response => response.json())
-//     .then(pokemon => {
-//             console.log(pokemon.abilities);
+async function getPokemon() {
+    const searchValue = document.getElementById("datatable-search-input").value;
+    console.log(searchValue)
+    await fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue.toString() + "/" )
+    .then(response => response.json())
+    .then(pokemon => {
+            console.log(pokemon.abilities);
 
             
-//             // for (let j = 0; j < data.results.length; j++) {
-//                 //     console.log(data.results[j].name)
-//                 const abilityContainer = document.querySelector(".ability-container")
-//                 const pokemonInfo = document.createElement("div")
-//                 abilityContainer.textContent = ""
-//                 pokemonInfo.setAttribute("class", "pokemoninfo")
-//                 // pokemonInfo.textContent = 
-//                 console.log(pokemonInfo)
-//                 abilityContainer.appendChild(pokemonInfo)
+            // for (let j = 0; j < data.results.length; j++) {
+                //     console.log(data.results[j].name)
+                const abilityContainer = document.querySelector(".ability-container")
+                const pokemonInfo = document.createElement("div")
+                abilityContainer.textContent = ""
+                pokemonInfo.setAttribute("class", "pokemoninfo")
+                // pokemonInfo.textContent = 
+                console.log(pokemonInfo)
+                abilityContainer.appendChild(pokemonInfo)
 
-//                 for (let i = 0; i < pokemon.abilities.length; i++) {
-//                     console.log(pokemon.abilities[i].ability.name);
-//                     const ability = document.createElement("p")
-//                     ability.textContent = pokemon.abilities[i].ability.name
-//                     pokemonInfo.appendChild(ability)
-//                 }
+                for (let i = 0; i < pokemon.abilities.length; i++) {
+                    console.log(pokemon.abilities[i].ability.name);
+                    const ability = document.createElement("p")
+                    ability.textContent = pokemon.abilities[i].ability.name
+                    pokemonInfo.appendChild(ability)
+                }
                 
                 
 
                 
-//         })
-//     }
+        })
+    }
 
-//     const pokemonContainer = document.querySelector(".ability-container");
-//     const formEl = document.querySelector("form");
-//     const inputEl = document.querySelector("input[type=text]");
+    const pokemonContainer = document.querySelector(".ability-container");
+    const formEl = document.querySelector("form");
+    const inputEl = document.querySelector("input[type=text]");
     
 
-// pokemonSearch.addEventListener("click", getPokemon);
+pokemonSearch.addEventListener("click", getPokemon);
--- assets/script.js/pokeTCG.js
+++
@@ -1,9 +0,0 @@
-// Second API Call
-async function testCall2() {
-    await fetch("https://api.pokemontcg.io/v2/cards")
-        .then(response => response.json())
-        .then(cards => console.log(cards))
-
-}
-
-testCall2();          
